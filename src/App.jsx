bf5c87160fee1e3ed8dc3304f561dfa3116d920a
Vaishnavi <vaishnavi293yadav@gmail.com>
2025-03-25 12:19:46 +0530

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ function App() {
   <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
+    <Route path='/contactus' element={<ContactUs/>}/>
+    <Route path='/directory' element={<Directory/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+    <Route path='/event' element={<Event/>}/>
+    <Route path='/signin' element={<SignIn/>}/>
+    <Route path='/signup' element={<SignUp/>}/>
   </Routes> 
   </BrowserRouter>
   )
