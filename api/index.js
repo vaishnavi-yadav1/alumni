cd9036404ad76932cfea270084a9ca3849ee0b6e
Vaishnavi <vaishnavi293yadav@gmail.com>
2025-03-25 22:18:06 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
- import express from 'express';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
- const app=express();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to db');
+}
+).catch((err) => {
+    console.log(err);
+});
+const app = express();
 
- app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('server running on port 3000!');
-    
- });
+
+});
