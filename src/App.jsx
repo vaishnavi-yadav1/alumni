baffde23260ab9c8fb0a6390695cc146794d00cf
Vaishnavi <vaishnavi293yadav@gmail.com>
2025-03-25 11:51:05 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 function App() {
   
 
   return (
-   <h1 className='bg-black' >hello world</h1>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+  </Routes> 
+  </BrowserRouter>
   )
 }
 
