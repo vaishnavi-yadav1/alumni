fb6865dd25292abce01e2cf04d6b16c22c0a6da8
Vaishnavi <vaishnavi293yadav@gmail.com>
2025-03-26 11:30:02 +0530

    create alumni model

---
+++ api/models/alumni.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const alumniSchema= new mongoose.Schema(
+    {
+        name:{type:String,required:true},
+        email:{type:String,required:true,unique:true},
+        graduationYear:{type:Number,required:true},
+        password: { type: String, required: true },
+        department:{type:String,required:true},
+        currentJob:{type:String},
+        company:{type:String},
+        industry:{type:String},
+        linkedin:{type:String},
+        experience:{type:Number,required:true},
+        profileImage:{type:String},
+
+    },
+    {timestamps:true}
+);
