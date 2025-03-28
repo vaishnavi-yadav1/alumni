dae73ded9ec899a18eda9f1e9e58469912a66c20
Vaishnavi <vaishnavi293yadav@gmail.com>
2025-03-28 12:29:15 +0530

    add redux perisit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { store } from './redux/store.js';
-import { Provider } from 'react-redux';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { store, persistor } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')).render(
-  
-    <Provider store={store}>
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
-  
+    </PersistGate>
+  </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import alumniReducer from './alumni/alumniSlice';
+import {persistReducer,persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { version } from 'mongoose';
 
+
+const rootReducer = combineReducers({alumni:alumniReducer})
+
+const persistConfig={
+  key:'root',
+  storage,
+  version:1,
+};
+
+const persistedReducer=persistReducer(persistConfig,rootReducer);
 export const store = configureStore({
-  reducer: {alumni : alumniReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware)=>
     getDefaultMiddleware({
         serializableCheck:false,
     }),
 });
+
+export const persistor=persistStore(store);
