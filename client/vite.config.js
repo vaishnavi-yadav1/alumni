489874b6132d664513a46c2a9c587c38ee1bc48f
Vaishnavi <vaishnavi293yadav@gmail.com>
2025-06-07 18:34:05 +0530

    change4

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,31 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-// export default defineConfig({
-//   server:{
-//     proxy:{
-//       '/api':{
-//         target: 'http://localhost:3000',
-//         secure:false,
-//       },
-//     },
-//   },
-//   plugins: [react(),tailwindcss()],
-
-// });
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  build: {
-    outDir: '../api/client/dist', // output build here!
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // This is only used during development
+        changeOrigin: true,
+        secure: false,
+      },
     },
+  },
+  build: {
+    outDir: 'dist', // default is 'dist'
   },
 });
